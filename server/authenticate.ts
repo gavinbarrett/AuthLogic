e7c73f7a10543086b97240d83fb314ae0b56b826
Gavin <gavinbrrtt@gmail.com>
2021-08-19 00:19:29 -0700

    Added more types.

--- server/authenticate.ts
+++ server/authenticate.ts
@@ -1,20 +1,21 @@
 import * as db from './database';
 import * as argon2 from 'argon2';
 import { QueryResult } from 'pg';
+import { Request, Response } from 'express';
 
-const addUser = async (username, password, email) => {
+const addUser = async (username: string, password: string, email: string) => {
 	const hashed_password: string = await argon2.hash(password);
 	const query: string = 'insert into users (username, password, email) values ($1, $2, $3)';
 	return db.queryValues(query, [username, hashed_password, email]);
 }
 
-const findUser = async (username) => {
+const findUser = async (username: string) => {
 	// look for user in the database
 	let query: string = "select username, password from users where username=$1";
 	return db.queryValues(query, [username]);
 }
 
-const parseCreds = (username, password, res) => {
+const parseCreds = (username: string, password: string, res: Response) => {
 	if (!username || !password || !username.match(/^[a-zA-Z0-9]{10,48}$/) || !password.match(/^[a-zA_Z0-9]{10,48}$/)) {
 		// check for undefined and regex match
 		res.status(401).end('incorrect credentials');
@@ -23,13 +24,13 @@ const parseCreds = (username, password, res) => {
 	return true;
 }
 
-export const signUp = async (req, res) => {
+export const signUp = async (req: Request, res: Response) => {
 	// receive username and password
 	const { username, password } = req.body;
 	// parse the credentials
 	if (!parseCreds(username, password, res)) return;
 	// find the user in the database
-	const exists = await findUser(username);
+	const exists: QueryResult = await findUser(username);
 	// if user exists
 	if (exists.rowCount) {
 		// user exists in the database
@@ -41,15 +42,15 @@ export const signUp = async (req, res) => {
 	}
 }
 
-export const signIn = async (req, res) => {
+export const signIn = async (req: Request, res: Response) => {
 	// receive username and password
 	const { username, password } = req.body;
 	// parse the credentials
 	if (!parseCreds(username, password, res)) return;
 	// find the user in the database
-	const result = await findUser(username);
+	const result: QueryResult = await findUser(username);
 	if (result.rowCount) {
-		const hashed_password = result.rows[0].password;
+		const hashed_password: string = result.rows[0].password;
 		const matched = await argon2.verify(hashed_password, password);
 		matched ? res.status(200).end('user authed') : res.status(401).end('password does not match');
 	} else {
